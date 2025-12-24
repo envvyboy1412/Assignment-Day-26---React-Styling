d0e193837bf301cea1ced93cd6691e74f1e25e15
Pedroni Gilbran <pedronibt45@gmail.com>
2025-12-24 10:01:32 +0700

    done about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -12,11 +12,11 @@ export default function About() {
         className="flex flex-col items-center justify-center gap-4 h-[70vh] bg-cover bg-center bg-no-repeat"
         style={{ backgroundImage: "url('/img/about/bg.webp')" }}
       >
-        <div className="text-2xl md:text-5xl text-center text-white">
+        <div className="text-2xl md:text-5xl text-center text-white" style={{ textShadow: "2px 2px 6px rgba(0,0,0,0.9)" }}>
           Your Neighbourhood Coffee Haven
         </div>
 
-        <div className="px-3 py-2 text-xs md:text-3xl lg:text-4xl xl:text-2xl text-center text-white max-w-2xl">
+        <div className="px-3 py-2 text-xs md:text-3xl lg:text-4xl xl:text-2xl text-center text-white max-w-2xl" style={{ textShadow: "2px 2px 6px rgba(0,0,0,0.9)" }}>
           Crafted with passion, served with a smile. Discover the story behind
           every cup
         </div>
